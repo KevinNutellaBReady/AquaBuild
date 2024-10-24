6125cd1c52b576674f10fcedd93e52cd115c4ed3
KevinNutellaBReady <kevin26032006@gmail.com>
2024-10-24 23:26:43 +0200

    + PerkInventory Items + Database Manager

--- src/main/java/me/kevin/citybuild/inventorys/PerkInventory.java
+++ src/main/java/me/kevin/citybuild/inventorys/PerkInventory.java
@@ -1,16 +1,52 @@
 package me.kevin.citybuild.inventorys;
 
+import me.kevin.citybuild.manager.ItemManager;
+import me.kevin.citybuild.manager.PerksDatabaseManager;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 public class PerkInventory {
 
     public static Inventory getPerkInventory(Player player) {
         Inventory inventory = player.getServer().createInventory(null, 45, "§a§lPerks");
 
-        inventory.addItem(new ItemStack(Material.BAKED_POTATO, 1));
+        inventory.setItem(18, new ItemManager("§b§lSpeed§r§7§l-§r§b§lPerk", Material.DIAMOND_BOOTS, (byte) 0, 1, "").build());
+        inventory.setItem(20, new ItemManager("§e§lHaste§r§7§l-§r§e§lPerk", Material.GOLDEN_PICKAXE, (byte) 0, 1, "").build());
+        inventory.setItem(22, new ItemManager("§b§lDoublejump§r§7§l-§r§b§lPerk", Material.DIAMOND_BOOTS, (byte) 0, 1, "").build());
+        inventory.setItem(24, new ItemManager("§c§lStrength§r§7§l-§r§b§lPerk", Material.IRON_SWORD, (byte) 0, 1, "").build());
+
+        /* Activate / Buy Item in GUI */
+
+        if(PerksDatabaseManager.ownSpeedPerk(player)) {
+            inventory.setItem(26, new ItemManager("§c§lDeaktiviert", Material.RED_DYE, (byte) 0, 1, "").build());
+
+        } else {
+            inventory.setItem(26, new ItemManager("§4§lNicht gekauft!", Material.BARRIER, (byte) 0, 1, "").build());
+        }
+
+        if(PerksDatabaseManager.ownHastePerk(player)) {
+            inventory.setItem(28, new ItemManager("§c§lDeaktiviert", Material.RED_DYE, (byte) 0, 1, "").build());
+
+        } else {
+            inventory.setItem(28, new ItemManager("§4§lNicht gekauft!", Material.BARRIER, (byte) 0, 1, "").build());
+        }
+
+        if(PerksDatabaseManager.ownDoubleJumpPerk(player)) {
+            inventory.setItem(28, new ItemManager("§c§lDeaktiviert", Material.RED_DYE, (byte) 0, 1, "").build());
+
+        } else {
+            inventory.setItem(28, new ItemManager("§4§lNicht gekauft!", Material.BARRIER, (byte) 0, 1, "").build());
+        }
+
+        if(PerksDatabaseManager.ownStrength(player)) {
+            inventory.setItem(30, new ItemManager("§c§lDeaktiviert", Material.RED_DYE, (byte) 0, 1, "").build());
+
+        } else {
+            inventory.setItem(30, new ItemManager("§4§lNicht gekauft!", Material.BARRIER, (byte) 0, 1, "").build());
+        }
+
+
 
         return inventory;
     }
--- src/main/java/me/kevin/citybuild/manager/PerksDatabaseManager.java
+++ src/main/java/me/kevin/citybuild/manager/PerksDatabaseManager.java
@@ -10,7 +10,7 @@ public class PerksDatabaseManager {
 
     public static void createTable(){
         try{
-            PreparedStatement ps = MySQL.getStatement("CREATE TABLE IF NOT EXISTS Perks (Spielername VARCHAR(100), UUID VARCHAR(100), Speed BOOLEAN, Haste BOOLEAN, DoubleJump BOOLEAN)");
+            PreparedStatement ps = MySQL.getStatement("CREATE TABLE IF NOT EXISTS Perks (Spielername VARCHAR(100), UUID VARCHAR(100), Speed BOOLEAN, Haste BOOLEAN, DoubleJump BOOLEAN, Strenght BOOLEAN)");
             ps.executeUpdate();
         }catch(Exception ex){
             ex.printStackTrace();
@@ -19,12 +19,13 @@ public class PerksDatabaseManager {
 
     public static void register(Player p){
         try{
-            PreparedStatement ps = MySQL.getStatement("INSERT INTO Perks (Spielername, UUID, Speed, Haste, DoubleJump) VALUES (?, ?, ?)");
+            PreparedStatement ps = MySQL.getStatement("INSERT INTO Perks (Spielername, UUID, Speed, Haste, DoubleJump, Strength) VALUES (?, ?, ?)");
             ps.setString(1, p.getName());
             ps.setString(2, p.getUniqueId().toString());
             ps.setBoolean(3, false);
             ps.setBoolean(4, false);
             ps.setBoolean(5, false);
+            ps.setBoolean(6, false);
             ps.executeUpdate();
             ps.close();
         }catch(Exception ex){
@@ -46,4 +47,69 @@ public class PerksDatabaseManager {
         }
         return false;
     }
+
+    public static boolean ownSpeedPerk(Player p){
+        try{
+            PreparedStatement ps = MySQL.getStatement("SELECT * FROM Perks WHERE UUID= ?");
+            ps.setString(1, p.getUniqueId().toString());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            boolean speed = rs.getBoolean("Speed");
+            rs.close();
+            ps.close();
+            return speed;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean ownHastePerk(Player p){
+        try{
+            PreparedStatement ps = MySQL.getStatement("SELECT * FROM Perks WHERE UUID= ?");
+            ps.setString(1, p.getUniqueId().toString());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            boolean haste = rs.getBoolean("Haste");
+            rs.close();
+            ps.close();
+            return haste;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean ownDoubleJumpPerk(Player p){
+        try{
+            PreparedStatement ps = MySQL.getStatement("SELECT * FROM Perks WHERE UUID= ?");
+            ps.setString(1, p.getUniqueId().toString());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            boolean doubleJump = rs.getBoolean("DoubleJump");
+            rs.close();
+            ps.close();
+            return doubleJump;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean ownStrength(Player p){
+        try{
+            PreparedStatement ps = MySQL.getStatement("SELECT * FROM Perks WHERE UUID= ?");
+            ps.setString(1, p.getUniqueId().toString());
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            boolean doubleJump = rs.getBoolean("Strength");
+            rs.close();
+            ps.close();
+            return doubleJump;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
 }
